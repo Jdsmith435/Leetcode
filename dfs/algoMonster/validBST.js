28c5dad625a1ac08a4505001e7eb15dc310875ef
jdsmith435 <jdsmith435@comcast.net>
2025-05-06 16:44:16 -0600

    fixes

--- dfs/algoMonster/validBST.js
+++ dfs/algoMonster/validBST.js
@@ -3,7 +3,7 @@
 function isValidBST(root) {
 	function dfs(node, min, max) {
 		if (!node) return true;
-		if (node.val < min && node.val > max) return false;
+		if (node.val <= min || node.val >= max) return false;
 		return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
 	}
 	return dfs(root, -Infinity, Infinity);
