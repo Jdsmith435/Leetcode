083221f6a23bcd378722e060f11fbd632939a912
jdsmith435 <jdsmith435@comcast.net>
2025-05-11 14:11:04 -0600

    finished pn comb prop code

--- backtrack/algomonster/phoneNumberCombination.js
+++ backtrack/algomonster/phoneNumberCombination.js
@@ -1,5 +1,16 @@
 //phone number combination
 
+const KEYBOARD = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+};
+
 function letterCombinations(digits: string): string[] {
     if(!digits) return [];
     const results = [];
