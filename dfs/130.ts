292dc63b27d2a1668d97279bdeaa0e274fc55c8e
Jeremiah Smith <jdsmith435@comcast.net>
2025-05-30 07:31:15 -0600

    Update 130.ts

--- dfs/130.ts
+++ dfs/130.ts
@@ -1,4 +1,6 @@
 // Surrounded regions. First impl.
+// This however will change the board on branches that are surrounded but 
+// if another branch is not surrourned, we cant undo those changes.
 function solve(board: string[][]): void {
     const height = board.length;
     const width = board[0].length;
@@ -33,3 +35,37 @@ function solve(board: string[][]): void {
         }
     }
 };
+
+// The right impl is to iterate on the boarder and mark those as safe. Then change all the
+// other O to X
+function solve(board: string[][]): void {
+    const m = board.length;
+    const n = board[0].length;
+
+    const dfs = (r: number, c: number) => {
+        if (r < 0 || c < 0 || r >= m || c >= n || board[r][c] !== 'O') return;
+
+        board[r][c] = 'T';
+
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
+    };
+
+    for (let i = 0; i < m; i++) {
+        if (board[i][0] === 'O') dfs(i, 0);
+        if (board[i][n - 1] === 'O') dfs(i, n - 1);
+    }
+    for (let j = 0; j < n; j++) {
+        if (board[0][j] === 'O') dfs(0, j);
+        if (board[m - 1][j] === 'O') dfs(m - 1, j);
+    }
+
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (board[r][c] === 'O') board[r][c] = 'X';
+            if (board[r][c] === 'T') board[r][c] = 'O'; 
+        }
+    }
+}
